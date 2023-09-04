2168893bfb0a042fca72c78c3d6a810aa6e34adf
YujinCho <22100708@handong.ac.kr>
2023-09-04 14:02:21 +0900

    Add listAll method (READ)

--- src/main/java/edu/handong/WordCRUD.java
+++ src/main/java/edu/handong/WordCRUD.java
@@ -1,4 +1,47 @@
 package edu.handong;
 
-public class WordCRUD {
+import java.util.ArrayList;
+import java.util.Scanner;
+public class WordCRUD implements  ICRUD{
+    ArrayList<Word> list;
+    Scanner s;
+
+    WordCRUD(Scanner s){
+        list = new ArrayList<>();
+        this.s = s;
+    }
+
+    // 1번
+    public void listAll() {
+        System.out.println("\n--------------------------------");
+        for(int i = 0; i < list.size(); i++) {
+            System.out.print(i+1 + " ");
+            System.out.println(list.get(i).toString());
+        }
+        System.out.println("--------------------------------\n");
+    }
+
+
+    // 4번
+    @Override
+    public Object add() {
+        return null;
+    }
+    public void addItem(){
+
+    }
+
+    @Override
+    public int update(Object obj) {
+        return 0;
+    }
+
+    @Override
+    public int delete(Object obj) {
+        return 0;
+    }
+
+    @Override
+    public void selectOne(int id) {
+    }
 }
--- src/main/java/edu/handong/WordManager.java
+++ src/main/java/edu/handong/WordManager.java
@@ -1,4 +1,44 @@
 package edu.handong;
 
+import java.util.Scanner;
 public class WordManager {
+    Scanner s = new Scanner(System.in);
+    WordCRUD wordCRUD;
+    WordManager(){
+        wordCRUD = new WordCRUD(s);
+    }
+
+    public void start() { // main 역할
+        while(true) {
+            int menu = selectMenu();
+            if (menu == 0) {
+                System.out.println("\n프로그램 종료! 다음에 만나요~");
+                break;
+            }
+            if (menu == 4) { // create
+                wordCRUD.addItem();
+            } else if (menu == 1) { // list
+                wordCRUD.listAll();
+            }
+        }
+    }
+
+    private int selectMenu() {
+        System.out.print("	 *** 영단어 마스터 ***\r\n"
+                + "*********************\r\n"
+                + "1. 모든 단어 보기\r\n"
+                + "2. 수준별 단어 보기\r\n"
+                + "3. 단어 검색\r\n"
+                + "4. 단어 추가\r\n"
+                + "5. 단어 수정\r\n"
+                + "6. 단어 삭제\r\n"
+                + "7. 파일 저장\r\n"
+                + "0. 나가기\r\n"
+                + "*********************\r\n"
+                + "=> 원하는 메뉴는? ");
+
+        return s.nextInt();
+    }
+
+
 }
