4ceee2343c00cd792e43392c16914af9c06b0af5
YujinCho <22100708@handong.ac.kr>
2023-09-04 14:05:21 +0900

    Add addItem method (CREATE)

--- src/main/java/edu/handong/WordCRUD.java
+++ src/main/java/edu/handong/WordCRUD.java
@@ -25,10 +25,19 @@ public class WordCRUD implements  ICRUD{
     // 4번
     @Override
     public Object add() {
-        return null;
+        System.out.print("\n=> 난이도(1,2,3) & 새 단어 입력: ");
+        int level = s.nextInt();
+        String word = s.nextLine();
+
+        System.out.print("=> 뜻 입력 : ");
+        String meaning = s.nextLine();
+
+        return new Word(0, level, word, meaning);
     }
     public void addItem(){
-
+        Word one = (Word)add();
+        list.add(one);
+        System.out.println("\n새 단어가 단어장에 추가되었습니다 !!\n");
     }
 
     @Override
