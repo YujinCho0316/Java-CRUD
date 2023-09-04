05807dd3464ed393c0b50bb5a2566c4ef5649fbb
YujinCho <22100708@handong.ac.kr>
2023-09-04 20:16:35 +0900

    Add updateItem method (UPDATE)

--- src/main/java/edu/handong/WordManager.java
+++ src/main/java/edu/handong/WordManager.java
@@ -19,6 +19,8 @@ public class WordManager {
                 wordCRUD.addItem();
             } else if (menu == 1) { // list
                 wordCRUD.listAll();
+            } else if (menu == 5) { // update
+                wordCRUD.updateItem();
             }
         }
     }
